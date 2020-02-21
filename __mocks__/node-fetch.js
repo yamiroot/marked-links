d1e2e4cda040456d796abcc88f2094c3dd4a01a2
yamiquispe <yamipqm@gmail.com>
2020-02-21 18:34:17 -0500

    Elaboración de mock manual para testeo

---
+++ __mocks__/node-fetch.js
@@ -0,0 +1,11 @@
+const nodeFetch = jest.requireActual('node-fetch');
+const fetchMock = require('fetch-mock').sandbox();
+
+Object.assign(fetchMock.config, {
+  fetch: nodeFetch,
+});
+
+fetchMock
+  .mock('*', 404);
+
+module.exports = fetchMock;
