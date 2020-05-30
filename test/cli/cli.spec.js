3438ffa1f9e158221d40fd6301d8280c6cd53da9
yamiquispe <yamipqm@gmail.com>
2020-05-30 04:39:21 -0500

    Test al 100% de archivo cli.js

--- test/cli/cli.spec.js
+++ test/cli/cli.spec.js
@@ -1,96 +1,82 @@
 const path = require('path');
 const colors = require('colors');
 
+
 const { cli } = require('../../lib/cli/cli');
-const { mdLinks } = require('../../lib/api/index');
 
 
-describe('Cli', () => {
-  const arrayReturn = [{
-    file: path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'),
-    href: 'https://dzone.com/articles/huwork',
-    text: 'SPA',
-  }];
+const informationLinkArchive = [{
+  href: 'https://dzone.com/articles/huwork',
+  text: 'SPA',
+  file: path.join(process.cwd(), 'MarkdownForTest', 'Readme.md'),
+}];
 
-  const arrayReturnStatus = [{
-    file: path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'),
-    href: 'https://dzone.com/articles/huwork',
-    text: 'SPA',
-    status: 404,
-    statusText: 'fail',
-  }];
+const informationLinkStatusArchive = [{
+  href: 'https://dzone.com/articles/huwork',
+  text: 'SPA',
+  file: path.join(process.cwd(), 'MarkdownForTest', 'Readme.md'),
+  status: 404,
+  statusText: 'fail',
+}];
 
-  it('Debería ser una función.', () => {
+
+describe('Testing de función cli', () => {
+  it('cli debería ser una función.', () => {
     expect(typeof cli).toBe('function');
   });
 
-  it('Debería retornar una promesa cuyo valor resuelto es: "Por favor, ingrese una ruta."',
+  it('Debería retornar un mensaje "Por favor, ingrese una ruta." al recibir como segundo argumento un array vacío.',
     (done) => cli([]).then((response) => {
-      expect(response).toBe(colors.america('Por favor, ingrese una ruta.'));
+      expect(response).toBe(colors.yellow('Por favor, ingrese una ruta.'));
       done();
     }));
 
-  it('Debería retornar una promesa cuyo valor resuelto es: "Ha excedido el número de comandos permitidos."',
+  it('Debería retornar un mensaje "Ha excedido el número de comandos permitidos." al recibir como argumento un array de más de tres elementos.',
     (done) => cli([1, 2, 3, 4]).then((response) => {
       expect(response).toBe(colors.dim('Ha excedido el número de comandos permitidos.'));
       done();
     }));
 
 
-  it('Debería retornar una promesa cuyo valor resuelto es: "Por favor, ingrese un comando."',
-    (done) => cli([path.join(process.cwd(), 'MarkdownForTests', 'Readme.md')]).then((response) => {
-      expect(response).toBe(colors.bold('Por favor, ingrese un comando.'));
+  it('Debería retornar un array de objetos con las propiedades: href, text y file, al recibir como argumento un array cuyo único elemento es la ruta válida de un archivo.',
+    (done) => cli([path.join(process.cwd(), 'MarkdownForTest', 'Readme.md')]).then((response) => {
+      expect(response).toStrictEqual(informationLinkArchive);
       done();
     }));
 
-
-  it('Pasamos como parámetro la ruta y "--stats". Por ello, debería retornar una promesa cuyo valor resuelto es un array de objetos con las propiedades href, text, y file.',
-    (done) => mdLinks(path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), { validate: false }).then((response) => {
-      cli([path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), '--stats']).then(() => {
-        expect(response).toStrictEqual(arrayReturn);
-      });
+  it('Debería retornar un array de objetos con las propiedades: href, text y file, al recibir como argumento un array cuyos elementos son la ruta válida de un archivo y "--stats".',
+    (done) => cli([path.join(process.cwd(), 'MarkdownForTest', 'Readme.md'), '--stats']).then((response) => {
+      expect(response).toStrictEqual(informationLinkArchive);
       done();
     }));
 
-
-  it('Pasamos como parámetro la ruta y "--validate". Por ello, debería retornar una promesa cuyo valor resuelto es un array de objetos con las propiedades href, text, file, status y statusText.',
-    (done) => mdLinks(path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), { validate: true }).then((response) => {
-      cli([path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), '--validate']).then(() => {
-        expect(response).toStrictEqual(arrayReturnStatus);
-      });
-
+  it('Debería retornar un array de objetos con las propiedades: href, text, file, status y statusText, al recibir como argumento un array cuyos elementos son la ruta válida de un archivo y "--validate".',
+    (done) => cli([path.join(process.cwd(), 'MarkdownForTest', 'Readme.md'), '--validate']).then((response) => {
+      expect(response).toStrictEqual(informationLinkStatusArchive);
       done();
     }));
 
-  it('Pasamos como parámetro la ruta, "--validate" y "--stats" (En ese orden). Por ello, debería retornar una promesa cuyo valor resuelto es un array de objetos con las propiedades href, text, file, status y statusText.',
-    (done) => mdLinks(path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), { validate: true }).then((response) => {
-      cli([path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), '--validate', '--stats']).then(() => {
-        expect(response).toStrictEqual(arrayReturnStatus);
-      });
-
+  it('Debería retornar un array de objetos con las propiedades: href, text, file, status y statusText, al recibir como argumento un array cuyos elementos son la ruta válida de un archivo, "--validate" y "--stats".',
+    (done) => cli([path.join(process.cwd(), 'MarkdownForTest', 'Readme.md'), '--validate', '--stats']).then((response) => {
+      expect(response).toStrictEqual(informationLinkStatusArchive);
       done();
     }));
 
-
-  it('Pasamos como parámetro la ruta, "--stats" y "--validate" (En ese orden). Por ello, debería retornar una promesa cuyo valor resuelto es un array de objetos con las propiedades href, text, file, status y statusText.',
-    (done) => mdLinks(path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), { validate: true }).then((response) => {
-      cli([path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), '--stats', '--validate']).then(() => {
-        expect(response).toStrictEqual(arrayReturnStatus);
-      });
-
+  it('Debería retornar un array de objetos con las propiedades: href, text, file, status y statusText, al recibir como argumento un array cuyos elementos son la ruta válida de un archivo, "--stats" y "--validate".',
+    (done) => cli([path.join(process.cwd(), 'MarkdownForTest', 'Readme.md'), '--stats', '--validate']).then((response) => {
+      expect(response).toStrictEqual(informationLinkStatusArchive);
       done();
     }));
 
-  it('Debería retornar una promesa cuyo valor resuelto es: "El comando ingresado no es válido."',
-    (done) => cli([path.join(process.cwd(), 'MarkdownForTests', 'Readme.md'), 'comando-invalido']).then((response) => {
-      expect(response).toBe(colors.rainbow('El comando ingresado no es válido.'));
+  it('Debería retornar un mensaje "El comando ingresado no es válido." al recibir como argumento un array cuyos elementos son la ruta válida de un archivo y un comando inválido.',
+    (done) => cli([path.join(process.cwd(), 'MarkdownForTest', 'Readme.md'), 'root']).then((response) => {
+      expect(response).toStrictEqual(colors.cyan('El comando ingresado no es válido.'));
       done();
     }));
 
-
-  it('Debería retornar una promesa fallida cuyo valor resuelto es: "La ruta ingresada no existe"',
+  it('Debería retornar un mensaje de error "La ruta ingresada no existe." al recibir como argumento un array cuyo elemento es una ruta inválida.',
     (done) => cli(['ruta-invalida']).catch((response) => {
-      expect(response).toStrictEqual(new Error(colors.cyan('La ruta ingresada no existe')));
+      expect(response).toStrictEqual(new Error(colors.magenta('La ruta ingresada no existe.')));
       done();
     }));
 });
